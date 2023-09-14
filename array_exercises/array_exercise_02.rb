e08171ee9e9cda16085a7a740e914f9bcf96b863
rextumlos <johnrex412@gmail.com>
2023-09-14 10:27:38 +0800

    Array Exercise 02 - Printing unique elements

---
+++ array_exercises/array_exercise_02.rb
@@ -0,0 +1,27 @@
+# John Rexpearl Tumlos
+# Array Exercise #02
+# Write a program to print all unique elements in an array.
+
+arr = [1, 1, 2, 3, 2, 2, 6]
+
+# SOLUTION
+duplicates = Array.new
+uniques = Array.new
+
+arr.each_with_index do |num1, index1|
+  arr.each_with_index do |num2, index2|
+    if index1 != index2
+      if num1 == num2 and !duplicates.include?(num2)
+        duplicates << num1
+      end
+    end
+  end
+end
+
+arr.each_with_index do | num, index|
+  if !duplicates.include?(num)
+    uniques << num
+  end
+end
+
+print "Unique numbers: #{uniques}"
