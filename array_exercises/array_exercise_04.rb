8ffa2fbaf9e3401b9ffe4d332826b0a8a7423f3a
rextumlos <johnrex412@gmail.com>
2023-09-15 12:38:45 +0800

    Transforming array exercise 04 solution into a method.

--- array_exercises/array_exercise_04.rb
+++ array_exercises/array_exercise_04.rb
@@ -6,27 +6,32 @@ matrix1 = [[5, 6], [7 ,8]]
 matrix2 = [[1, 2], [3, 4]]
 
 # SOLUTION
-diff_matrix = Array.new
-matrix1.each_with_index do |arr1, index1|
-  matrix2.each_with_index do | arr2, index2|
-    if index1 == index2
-      diff_arr = Array.new
-      arr1.each_with_index do | num1, num_index1|
-        arr2.each_with_index do | num2, num_index2|
-          if num_index1 == num_index2
-            diff_arr << num1 - num2
+def subtraction_of_matrices(matrix1, matrix2)
+  diff_matrix = Array.new
+  matrix1.each_with_index do |arr1, index1|
+    matrix2.each_with_index do | arr2, index2|
+      if index1 == index2
+        diff_arr = Array.new
+        arr1.each_with_index do | num1, num_index1|
+          arr2.each_with_index do | num2, num_index2|
+            if num_index1 == num_index2
+              diff_arr << num1 - num2
+            end
           end
         end
+        diff_matrix << diff_arr
       end
-      diff_matrix << diff_arr
     end
   end
+
+  str_matrix = Array.new
+  diff_matrix.each do | arr |
+    str_matrix << arr.join(" ")
+    str_matrix << "\n"
+  end
+
+  str_matrix.join("")
 end
 
-str_matrix = Array.new
-diff_matrix.each do | arr |
-  str_matrix << arr.join(" ")
-  str_matrix << "\n"
-end
 
-print str_matrix.join("")
+print subtraction_of_matrices(matrix1, matrix2)
