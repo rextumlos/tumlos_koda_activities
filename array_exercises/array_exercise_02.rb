e175ef0429997845bb3b2af029c40f18e2f2e9b9
rextumlos <johnrex412@gmail.com>
2023-09-15 12:37:46 +0800

    Transforming array exercise 02 solution into a method.

--- array_exercises/array_exercise_02.rb
+++ array_exercises/array_exercise_02.rb
@@ -5,23 +5,27 @@
 arr = [1, 1, 2, 3, 2, 2, 6]
 
 # SOLUTION
-duplicates = Array.new
-uniques = Array.new
+def get_uniques(arr)
+  duplicates = Array.new
+  uniques = Array.new
 
-arr.each_with_index do |num1, index1|
-  arr.each_with_index do |num2, index2|
-    if index1 != index2
-      if num1 == num2 and !duplicates.include?(num2)
-        duplicates << num1
+  arr.each_with_index do |num1, index1|
+    arr.each_with_index do |num2, index2|
+      if index1 != index2
+        if num1 == num2 and !duplicates.include?(num2)
+          duplicates << num1
+        end
       end
     end
   end
-end
 
-arr.each_with_index do | num, index|
-  if !duplicates.include?(num)
-    uniques << num
+  arr.each_with_index do |num, index|
+    if !duplicates.include?(num)
+      uniques << num
+    end
   end
+
+  uniques
 end
 
-print "Unique numbers: #{uniques}"
+print "Unique numbers: #{get_uniques(arr)}"
