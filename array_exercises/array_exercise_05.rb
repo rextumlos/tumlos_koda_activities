88aa381c7bbb7b764d017b5b157f3ed55b2cace2
rextumlos <johnrex412@gmail.com>
2023-09-15 12:39:10 +0800

    Transforming array exercise 05 solution into a method.

--- array_exercises/array_exercise_05.rb
+++ array_exercises/array_exercise_05.rb
@@ -4,14 +4,18 @@
 orig_array = [23, -2, 45, 38, 12, 4, 6]
 
 # SOLUTION
-sorted_arr = orig_array.sort
-puts "Sorted Array: #{sorted_arr}"
+def largest_gap(array)
+  sorted_arr = array.sort
+  puts "Sorted Array: #{sorted_arr}"
 
-diff_arr = Array.new
-sorted_arr.each_with_index do | num, index|
-  if sorted_arr[index + 1]
-    diff_arr << sorted_arr[index + 1] - num
+  diff_arr = Array.new
+  sorted_arr.each_with_index do | num, index|
+    if sorted_arr[index + 1]
+      diff_arr << sorted_arr[index + 1] - num
+    end
   end
+
+  diff_arr.max
 end
 
-p diff_arr.max()
+p largest_gap(orig_array)
