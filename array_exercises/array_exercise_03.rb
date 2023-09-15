892e5ab54c07e224e3ca9089b6a5d88f448b05d4
rextumlos <johnrex412@gmail.com>
2023-09-15 12:38:20 +0800

    Transforming array exercise 03 solution into a method.

--- array_exercises/array_exercise_03.rb
+++ array_exercises/array_exercise_03.rb
@@ -2,13 +2,17 @@
 # Array Exercise #03
 # Write a program for a 2D array of size 3x3 and print the matrix.
 
-matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+array = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
 # SOLUTION
-str_matrix = Array.new
-matrix.each do | arr |
-  str_matrix << arr.join(" ")
-  str_matrix << "\n"
+def matrix(array)
+  str_matrix = Array.new
+  array.each do | arr |
+    str_matrix << arr.join(" ")
+    str_matrix << "\n"
+  end
+
+  str_matrix.join("")
 end
 
-print str_matrix.join("")
+print matrix(array)
