30343a1e5703c8177119e45ef71be1b6e02cb53a
rextumlos <johnrex412@gmail.com>
2023-09-18 15:07:42 +0800

    Exercise 5: Majority Element

---
+++ ruby-exercises/exercise_5.rb
@@ -0,0 +1,20 @@
+# John Rexpearl Tumlos
+# Exercise 5: Majority Element
+
+input_1 = [3, 2, 3]
+input_2 = [2, 2, 1, 1, 1, 2, 2]
+
+def majorityElement(nums)
+  hash_counter = Hash.new
+  nums.each do |num|
+    if hash_counter.include?(num)
+      hash_counter[num] += 1
+    else
+      hash_counter[num] = 1
+    end
+  end
+
+  print hash_counter.max_by{|key, value| value}[0]
+end
+
+majorityElement(input_2)
