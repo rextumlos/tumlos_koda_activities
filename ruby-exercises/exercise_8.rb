a4088aca30fd3eba137c724a502eaf1d08d7bf4c
rextumlos <johnrex412@gmail.com>
2023-09-19 09:16:45 +0800

    Exercise 8: Search Insert Position in Sorted Array

---
+++ ruby-exercises/exercise_8.rb
@@ -0,0 +1,24 @@
+# John Rexpearl Tumlos
+# Exercise 8 - Search Insert Position in Sorted Array
+
+input = [1, 3, 5, 6]
+target_1 = 5
+target_2 = 2
+target_3 = 7
+
+def searchInsert(nums, target)
+  if nums.include?(target)
+    nums.find_index(target)
+  else
+    nums.each_with_index do |num, index|
+      if target < num
+        return index
+      elsif nums.length - 1 == index
+        return nums.length
+      end
+    end
+  end
+end
+
+print searchInsert(input, target_3)
+
