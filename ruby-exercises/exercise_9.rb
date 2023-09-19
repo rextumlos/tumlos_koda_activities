4397fa5cca6caefc9c5fbf9126eef3ddc67985b8
rextumlos <johnrex412@gmail.com>
2023-09-19 13:08:17 +0800

    Exercise 9: Best Time to Buy and Sell Stock

---
+++ ruby-exercises/exercise_9.rb
@@ -0,0 +1,33 @@
+# John Rexpearl Tumlos
+# Exercise 9 - Best Time to Buy and Sell Stock
+
+input_1 = [7, 1, 5, 3, 6, 4]
+input_2 = [7, 6, 4, 3, 1]
+
+def maxProfit(prices)
+  low_price = nil
+  high_price = nil
+
+  prices.each_with_index do |price, index|
+    unless low_price and high_price
+      low_price = price
+      high_price = price
+    end
+
+    if price < low_price
+      low_price = price
+      high_price = 0
+    end
+
+    if price > high_price
+      high_price = price
+    end
+
+    puts "Loop #{index} - High: #{high_price}, Low: #{low_price}"
+  end
+
+  profit = high_price - low_price
+  profit < 0 ? 0 : profit
+end
+
+print maxProfit(input_2)
