882e797650663532278e149104818a2761064024
rextumlos <johnrex412@gmail.com>
2023-09-20 17:36:33 +0800

    Exercise 1: Length of Last Word

--- ruby-exercises/exercise_01.rb
+++ ruby-exercises/exercise_01.rb
@@ -1,13 +1,13 @@
 # John Rexpearl Tumlos
 # Exercise 1: Length of Last Word
 
-input_1 = "Hello World"
-input_2 = "   fly me   to   the moon  "
-input_3 = "luffy is still joyboy"
-
 def lengthOfLastWord(input)
   str_array = input.split(" ")
   puts "The last word is \"#{str_array[-1]}\" with length #{str_array[-1].length}."
 end
 
+
+input_1 = "Hello World"
+input_2 = "   fly me   to   the moon  "
+input_3 = "luffy is still joyboy"
 lengthOfLastWord(input_2)
